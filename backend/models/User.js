4b2d088581cd4598dd9952828f4147a97b7f4349
Sadre <mrsadre20@gmail.com>
2025-05-28 19:33:25 +0530

    artist

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,120 +1,128 @@
 import mongoose from "mongoose";
 import validator from "validator";
 
-const schema = new mongoose.Schema({
+const schema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Name is required"],
-        trim: true,
-        minlength: [2, "Name must be at least 2 characters long"]
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [2, "Name must be at least 2 characters long"],
     },
     email: {
-        type: String,
-        required: [true, "Email is required"],
-        unique: true,
-        validate: {
-            validator: validator.isEmail,
-            message: "Please enter a valid email"
-        }
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Please enter a valid email",
+      },
     },
-  password: {
-  type: String,
-  minlength: [8, "Password must be at least 8 characters long"],
-  select: false,
-  required: function() {
-    return !this.googleId; // agar Google login hai to password required nahi
-  }
-},
-googleId: {
-  type: String, // store Google profile id if you want
-},
+    password: {
+      type: String,
+      minlength: [8, "Password must be at least 8 characters long"],
+      select: false,
+      required: function () {
+        return !this.googleId;
+      },
+    },
+    googleId: {
+      type: String,
+    },
 
-facebookId:{
-  type: String,
-},
-appleId:{
-  type: String,
-},
+    facebookId: {
+      type: String,
+    },
+    appleId: {
+      type: String,
+    },
 
     resetPasswordToken: {
-    type: String,
-},
-resetPasswordExpire: {
-    type: Date,
-},
+      type: String,
+    },
+    resetPasswordExpire: {
+      type: Date,
+    },
 
     dob: {
-        type: Date,
+      type: Date,
     },
     role: {
-        type: String,
-        enum: ["user", "admin"],
-        default: "user"
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
     profileImage: {
-        type: String,
-        default: "",
+      type: String,
+      default: "",
     },
     playlist: [
-        {
-            type: String,
-            required: true,
-        },
+      {
+        type: String,
+        required: true,
+      },
     ],
     likedsong: [
-        {
-            type: String,
-            required: true,
-        },
+      {
+        type: String,
+        required: true,
+      },
     ],
 
-     preferredGenres: [
-        {
-            type: String,
-            trim: true,
-        },
+    preferredGenres: [
+      {
+        type: String,
+        trim: true,
+      },
     ],
-    
+
     purchasedSongs: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Song",
-        },
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song",
+      },
     ],
     purchasedAlbums: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Album",
-        },
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Album",
+      },
+    ],
+    subscribedArtists: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Artist",
+      },
     ],
 
     purchaseHistory: [
-  {
-    itemType: {
-      type: String,
-      enum: ["song", "album"],
-      required: true,
-    },
-    itemId: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      refPath: "purchaseHistory.itemType",
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    paymentId: {
-      type: String,
-      required: true,
-    },
-    purchasedAt: {
-      type: Date,
-      default: Date.now,
-    },
+      {
+        itemType: {
+          type: String,
+          enum: ["song", "album"],
+          required: true,
+        },
+        itemId: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          refPath: "purchaseHistory.itemType",
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        paymentId: {
+          type: String,
+          required: true,
+        },
+        purchasedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
   },
-],
-
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", schema);
