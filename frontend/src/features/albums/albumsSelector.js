5cd7da317823c3cb1e7f62031bd29388f5776cfd
reset networks03 <resetstudios03@resets-Mac-mini-2.local>
2025-06-13 16:17:18 +0530

    albums update

--- frontend/src/features/albums/albumsSelector.js
+++ frontend/src/features/albums/albumsSelector.js
@@ -1,5 +1,6 @@
 export const selectAllAlbums = (state) => state.albums.allAlbums;
 export const selectAlbumForm = (state) => state.albums.albumForm;
+export const selectAlbumById = (state, albumId) => state.albums.albumById?.[albumId];
 export const selectEditingAlbum = (state) => state.albums.editingAlbum;
 export const selectAlbumsLoading = (state) => state.albums.loading;
 export const selectAlbumsError = (state) => state.albums.error;
