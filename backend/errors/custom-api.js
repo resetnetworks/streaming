6551aac2b53a2b183d3121874007210d2a429469
sadre <mrsadre20@gmail.com>
2025-08-13 15:03:42 +0530

    backend remove

--- backend/errors/custom-api.js
+++
@@ -1,9 +0,0 @@
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "CustomAPIError";
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
-
-export default CustomAPIError;
--- backend/routes/discoverRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRandomArtistWithSongs } from "../controllers/discoverController.js";
-
-const router = express.Router();
-
-router.get("/random-artist", getRandomArtistWithSongs);
-
-export default router;
