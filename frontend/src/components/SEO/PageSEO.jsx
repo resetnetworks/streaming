80e9e4a0bf584daafc761595b994dcae45d358ee
sadre <mrsadre20@gmail.com>
2025-10-06 12:47:19 +0530

    react helmet async

--- frontend/src/components/SEO/PageSEO.jsx
+++ frontend/src/components/SEO/PageSEO.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import {Helmet} from "react-helmet-async";
 
 const PageSEO = ({ 
   title = "Reset Streaming Platform - Reset Music | Instrumental Music",
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,13 +6,16 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store, persistor } from "./app/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
- 
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  
+  <StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </HelmetProvider>
+  </StrictMode>
 );
