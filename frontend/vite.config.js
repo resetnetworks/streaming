a20441f48d293ae937d6cbe80c61e192fd02c3d8
sadre <mrsadre20@gmail.com>
2025-10-24 11:55:08 +0530

    vite prerender plugin uninstalled

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { vitePrerenderPlugin } from 'vite-prerender-plugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePrerenderPlugin({
-      renderTarget: '#root',
-      routes: ['/', '/contact-us', '/about-us','privacy-policy','terms-and-conditions','cancellation-refund-policy','data-deletion'],
-    })
-  ]
-})
+  plugins: [react()],
+});
