826d2eef478a728612d39a43469f4ee17858ab5d
sadre <mrsadre20@gmail.com>
2026-01-05 15:38:06 +0530

    test commit

--- frontend/src/utills/axiosInstance.js
+++ frontend/src/utills/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // ✅ Create axios instance
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL|| "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
   withCredentials: true, // ✅ Send cookies (needed for cookie-based auth)
   timeout: 300000,
 });
